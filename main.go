cdc265dca4c5bdae1611d159d5c4b0b8fea71fdb
Vincent <thisoneis4business@gmail.com>
2023-07-10 00:32:29 +1200

    chore: Get a block UI ready for list of members + action

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"d-exclaimation.me/relax/config"
+	"d-exclaimation.me/relax/lib/f"
 	"d-exclaimation.me/relax/lib/team"
 	"github.com/slack-go/slack"
 )
@@ -21,16 +23,59 @@ func main() {
 		return
 	}
 
-	attachments := make([]slack.Attachment, len(teamMembers))
+	content := make([]slack.Block, len(teamMembers)*3+2+1)
+
+	content[0] = slack.NewSectionBlock(
+		slack.NewTextBlockObject("mrkdwn", "Here’s the list of members of *Saturday* :saturday:", false, false),
+		nil,
+		nil,
+	)
+	content[1] = slack.NewDividerBlock()
 
 	for i, member := range teamMembers {
-		attachments[i] = slack.Attachment{
-			Text:  member.Profile.RealName,
-			Color: "#6366F1",
-		}
+		offset := i*3 + 2
+		content[offset] = slack.NewSectionBlock(
+			slack.NewTextBlockObject(
+				"mrkdwn",
+				fmt.Sprintf(
+					"*<github.com/d-exclaimation/relax|%s>*\n%s\n>:microservices: Has done *%d* review(s)\n>:hourglass:%s",
+					member.Profile.RealName,
+					f.IfElse(member.Profile.StatusText != "", member.Profile.StatusText, "_No status_"),
+					0,
+					member.TZLabel,
+				),
+				false,
+				false,
+			),
+			nil,
+			slack.NewAccessory(
+				slack.NewImageBlockElement(
+					member.Profile.Image72,
+					member.Profile.RealName,
+				),
+			),
+		)
+		content[offset+1] = slack.NewContextBlock(
+			"",
+			slack.NewImageBlockElement("https://w7.pngwing.com/pngs/195/984/png-transparent-slack-logo-icon-thumbnail.png", member.Profile.RealName),
+			slack.NewTextBlockObject("mrkdwn", member.Name, false, false),
+		)
+		content[offset+2] = slack.NewDividerBlock()
 	}
 
-	_, _, err = client.PostMessage(config.Env.Channels()[0], slack.MsgOptionAttachments(attachments...))
+	content[len(content)-1] = slack.NewActionBlock(
+		"",
+		slack.NewButtonBlockElement(
+			"random",
+			"random",
+			slack.NewTextBlockObject("plain_text", "Random", false, false),
+		),
+	)
+
+	_, _, err = client.PostMessage(
+		config.Env.Channels()[0],
+		slack.MsgOptionBlocks(content...),
+	)
 
 	if err != nil {
 		log.Fatalf("Cannot post message because %s\n", err)
